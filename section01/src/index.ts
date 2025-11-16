017dc6e9b372afeb4ecb9a070a5fa15e3e4fa8b3
OllyDI <choiseungho11@hanmail.net>
2025-11-16 15:41:11 +0900

    25.11.16

---
+++ section01/src/index.ts
@@ -0,0 +1,10 @@
+// import { hello } from './hello'
+
+// hello();
+// console.log("hello Typescript");
+// const a: number = 1;
+// const func = () => console.log("test");
+
+
+const a = 1;
+// export {};  // export/import 를 사용하면 전역변수 공유X 독립된/격리된 모듈이 됨
